06d3e9db967ddecef84104f2e60b3773d72b8704
Maxime Rossier <rossier.maxime@gmail.com>
2017-07-21 16:29:35 +0200

    add comments layout

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,10 +1,28 @@
 <template>
   <div class="mdl-card mdl-shadow--2dp">
-    <div class="mdl-card__title">
+    <div class="mdl-card__supporting-text">
       <h2 class="mdl-card__title-text">{{post.title}}</h2>
+      <p>{{post.details}}</p>
+      <p>Comments</p>
+      <template v-for="comment in comments">
+        <p>{{ comment.msg }}</p>
+      </template>
+      <form action="#">
+        <div class="mdl-textfield mdl-js-textfield">
+          <textarea class="mdl-textfield__input"
+            type="text"
+            rows= "5"
+            maxlength="200"
+            v-model="newComment"></textarea>
+          <label class="mdl-textfield__label" v-if="!newComment">Comment...</label>
+        </div>
+      </form>
     </div>
-    <div class="mdl-card__supporting-text">{{post.details}}</div>
     <div class="mdl-card__actions mdl-card--border">
+      <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect"
+        @click="sendComment">
+        {{commentButtonText}}
+      </a>
       <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect"
         @click="$emit('close')">
         Close
@@ -16,6 +34,16 @@
 <script>
 export default {
   name: 'post',
-  props: ['post']
+  props: ['post'],
+  data: function () {
+    return {
+      newComment: '',
+      commentButtonText: 'send',
+      comments: [{msg: 'aaa'}, {msg: 'lll'}]
+    }
+  },
+  methods: {
+    sendComment () {}
+  }
 }
 </script>
